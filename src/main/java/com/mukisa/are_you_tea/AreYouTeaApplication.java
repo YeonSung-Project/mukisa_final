d6e80e336e3c23995c5887b57880dad6d662d913
parkinhak <dlsgkr2341@naver.com>
2023-11-08 12:13:15 +0900

    스프링 시작파일 추가

---
+++ src/main/java/com/mukisa/are_you_tea/AreYouTeaApplication.java
@@ -0,0 +1,13 @@
+package com.mukisa.are_you_tea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreYouTeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreYouTeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mukisa/are_you_tea/AreYouTeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mukisa.are_you_tea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AreYouTeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
