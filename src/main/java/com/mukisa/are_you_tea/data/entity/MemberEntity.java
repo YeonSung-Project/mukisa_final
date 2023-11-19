0677d084bd863da814e9c7eb2cbe485af32128a0
youil <parkyouil98@gmail.com>
2023-11-19 22:26:13 +0900

    설명주석 추가

--- src/main/java/com/mukisa/are_you_tea/data/entity/MemberEntity.java
+++ src/main/java/com/mukisa/are_you_tea/data/entity/MemberEntity.java
@@ -12,17 +12,17 @@ public class MemberEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "MB_NO")
-    private int mbNo;
+    private int mbNo;           // 회원 고유 번호
     @Column(name = "MB_ID", unique = true)
-    private String mbId;
+    private String mbId;        // 회원 아이디
     @Column(name = "MB_PW")
-    private String mbPw;
+    private String mbPw;        // 회원 비밀번호
     @Column(name = "MB_NM")
-    private String mbNm;
+    private String mbNm;        // 회원 닉네임
     @Column(name = "MB_DATE")
-    private Date mbDate;
+    private Date mbDate;        // 회원 가입일
     @Column(name = "MB_YN")
-    private String mbYn = "N";
+    private String mbYn = "N";  // 회원 탈퇴 여부
     @Column(name = "MB_NDATE")
-    private Date mbNdate;
+    private Date mbNdate;       // 회원 탈퇴일자
 }
