10e27676cc176026beb15460d2a3772d435a0b67
parkinhak <dlsgkr2341@naver.com>
2023-12-07 15:04:01 +0900

    창일형 충돌 해결 추가

--- src/main/java/com/mukisa/are_you_tea/controller/MainController.java
+++ src/main/java/com/mukisa/are_you_tea/controller/MainController.java
@@ -2,15 +2,18 @@ package com.mukisa.are_you_tea.controller;
 
 import com.mukisa.are_you_tea.config.auth.PrincipalDetailsService;
 import com.mukisa.are_you_tea.data.entity.MemberEntity;
+import com.mukisa.are_you_tea.data.entity.RecipeEntity;
 import com.mukisa.are_you_tea.data.entity.UserEntity;
 import com.mukisa.are_you_tea.data.repository.MemberRepository;
 import com.mukisa.are_you_tea.data.repository.UserRepository;
+import com.mukisa.are_you_tea.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -23,6 +26,8 @@ public class MainController {
     private PrincipalDetailsService principalDetailsService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RecipeService recipeService;
 
     @GetMapping("/")
     public String mainPageController(Model model) {
@@ -36,6 +41,26 @@ public class MainController {
             if(user != null){
                 model.addAttribute("member", user.getUsername());
             }
+        }
+        //        메인 페이지 레시피 불러오기
+        try {
+            List<RecipeEntity> dataset = recipeService.dataLoad();
+
+            if (!dataset.isEmpty()) {
+                // 데이터셋이 5개 이상인 경우에만 상위 5개만 선택하여 모델에 추가
+                if (dataset.size() >= 5) {
+                    List<RecipeEntity> top5Recipes = dataset.subList(0, 4);
+                    model.addAttribute("recipeData", top5Recipes);
+                } else {
+                    // 데이터셋이 5개 미만인 경우 전체 데이터를 모델에 추가
+                    model.addAttribute("recipeData", dataset);
+                }
+            } else {
+                System.out.println("데이터셋이 비어 있습니다.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return "main";
     }
