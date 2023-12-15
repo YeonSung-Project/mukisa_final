56f2be8c8307723cce7cf982c3c79e78ef84aa81
parkinhak <dlsgkr2341@naver.com>
2023-12-15 20:08:42 +0900

    공지사항 페이징 제대로 만들어둠

---
+++ src/main/java/com/mukisa/are_you_tea/data/repository/NoticeRepository.java
@@ -0,0 +1,12 @@
+package com.mukisa.are_you_tea.data.repository;
+
+import com.mukisa.are_you_tea.data.entity.NoticeEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<NoticeEntity, Integer> {
+    Page<NoticeEntity> findByNoTitleContaining(String searchKeyword, Pageable pageable);
+}
