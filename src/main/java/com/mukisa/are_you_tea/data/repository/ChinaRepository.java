6e8e619c0a7d885f655fcdcc32ca5b64bf212c18
Chang1l <4718794@naver.com>
2023-12-17 20:15:59 +0900

    중국차 헬프

--- src/main/java/com/mukisa/are_you_tea/data/repository/ChinaRepository.java
+++ src/main/java/com/mukisa/are_you_tea/data/repository/ChinaRepository.java
@@ -1,13 +1,16 @@
 package com.mukisa.are_you_tea.data.repository;
 
 import com.mukisa.are_you_tea.data.entity.ChinaEntity;
-import org.springframework.stereotype.Repository;
+import com.mukisa.are_you_tea.data.entity.RecipeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ChinaRepository extends JpaRepository<ChinaEntity, Integer> {
-
-
 
 
 }
--- src/main/java/com/mukisa/are_you_tea/service/ChinaService.java
+++ src/main/java/com/mukisa/are_you_tea/service/ChinaService.java
@@ -1,14 +1,24 @@
 package com.mukisa.are_you_tea.service;
 
+import com.mukisa.are_you_tea.data.entity.ChinaEntity;
 import com.mukisa.are_you_tea.data.repository.ChinaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ChinaService {
 
     @Autowired
-    ChinaRepository chinaRepository;
+    private ChinaRepository chinaRepository;
+
+    public List<ChinaEntity> dataLoad() {
+        return chinaRepository.findAll();
+    }
+
+
+
 
 
 }
