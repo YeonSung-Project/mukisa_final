1fd95f64a3cf71973ea718a884c6ae201ae10eba
Chang1l <4718794@naver.com>
2023-12-18 16:31:55 +0900

    약차 리스트, 디테일 추가

--- src/main/java/com/mukisa/are_you_tea/controller/MedicineController.java
+++ src/main/java/com/mukisa/are_you_tea/controller/MedicineController.java
@@ -1,14 +1,92 @@
 package com.mukisa.are_you_tea.controller;
 
+import com.mukisa.are_you_tea.data.entity.MedicineEntity;
+import com.mukisa.are_you_tea.service.AdminCheckService;
+import com.mukisa.are_you_tea.service.MedicineService;
+import com.mukisa.are_you_tea.service.SessionCheckService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 
-// 약차 관련 페이지 컨트롤러 클래스입니다.
+
 @Controller
 public class MedicineController {
-    @GetMapping("/medicine")
-    public String medicineController(){
+    @Autowired
+    MedicineService medicineService;
+    @Autowired
+    private HttpSession httpSession;
+    @Autowired
+    private SessionCheckService sessionCheckService;
+    @Autowired
+    AdminCheckService adminCheckService;
+
+    @RequestMapping("/medicine")
+    public String gochinaList(@RequestParam(name = "recipeType", required = false) String recipeType,
+                              Model model, HttpServletRequest request
+    ) {
+        try {
+
+            List<MedicineEntity> recipeData;
+
+            sessionCheckService.sessionCheck(model, httpSession);
+
+            if (recipeType != null && !recipeType.isEmpty()) {
+
+                recipeData = medicineService.getRecipesByType(recipeType);
+            } else {
+
+                recipeData = medicineService.dataLoad();
+            }
+            model.addAttribute("recipeData", recipeData);
+
+            model.addAttribute("RecipeTypes", medicineService.getDistinctRecipeTypes());
+
+
+            model.addAttribute("selectedRecipeType", recipeType);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return "medicine";
     }
+
+
+
+    @RequestMapping("/medicineDetail")
+    public String gorecipeDetail(@RequestParam(name = "teano", required = false) Integer teano, Model model) {
+        try {
+            sessionCheckService.sessionCheck(model, httpSession);
+
+
+            if (teano != null) {
+                MedicineEntity recipe = medicineService.findChinaEntityById(teano);
+                if (recipe != null) {
+                    model.addAttribute("recipeDetailData", recipe);
+
+
+                } else {
+
+                }
+            } else {
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+
+        return "medicineDetail";
+    }
+
+
 }
--- src/main/java/com/mukisa/are_you_tea/service/MedicineService.java
+++ src/main/java/com/mukisa/are_you_tea/service/MedicineService.java
@@ -1,14 +1,38 @@
 package com.mukisa.are_you_tea.service;
 
+import com.mukisa.are_you_tea.data.entity.MedicineEntity;
 import com.mukisa.are_you_tea.data.repository.MedicineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MedicineService {
 
     @Autowired
-    MedicineRepository MedicineRepository;
+    MedicineRepository medicineRepository;
 
+
+    public List<MedicineEntity> dataLoad() {
+        return medicineRepository.findAll();
+    }
+
+    // 레시피 타입으로 레시피 조회
+    public List<MedicineEntity> getRecipesByType(String recipeType) {
+        return medicineRepository.findByTeaCategory(recipeType);
+    }
+
+    // ID로 레시피 조회
+    public MedicineEntity findChinaEntityById(int teano) {
+        Optional<MedicineEntity> chinaOptional = medicineRepository.findById(teano);
+        return chinaOptional.orElse(null);
+    }
+
+    // 레시피 타입 가져오기
+    public List<String> getDistinctRecipeTypes() {
+        return medicineRepository.findDistinctByTeaCategory();
+    }
 
 }
